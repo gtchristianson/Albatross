2009739656316d2b844bb4ab5606cb3f44a204f7
JosephBillstrom <joseph.billstrom@ndsu.edu>
2016-04-07 18:22:47 -0500

    Start Menu
    A simple start menu, with Play and Quit. The play button script is
    written to have the SceneManager load scene(1), which should correspond
    to level one. This start menu should be consider Level 0.

---
+++ Albatross_MainMenu/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseHover : MonoBehaviour {
+
+    public Renderer rend;
+	// Use this for initialization
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color.white;
+	}
+
+    void OnMouseEnter()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color.cyan;
+    }
+	
+    void OnMouseExit()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color.white;
+    }
+
+}
